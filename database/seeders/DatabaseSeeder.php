3d3ef8e7563866e6d93053e7d00ce9e97ef08e2c
Ahmad Fernando <ahmadfernando88@gmail.com>
2025-06-04 20:36:15 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,13 +23,21 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'admin',
         //     'username' => 'admin',
-        //     'email' => 'admint@example.com',
-        //     'password' => bcrypt('password'),
+        //     'email' => 'admin@example.com',
+        //     'role' => 'admin',
+        //     'password' => bcrypt('123'),
+        // ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'superadmin',
+        //     'username' => 'superadmin',
+        //     'email' => 'superadmin@example.com',
+        //     'role' => 'super admin',
+        //     'password' => bcrypt('123'),
         // ]);
         $this->call([
             // JenisBarangSeeder::class,
             // MerkSeeder::class,
-            AsetSeeder::class
+            // AsetSeeder::class
         ]);
     }
 }
