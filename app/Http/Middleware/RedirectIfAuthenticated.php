5745abbc1101fea6348b4d4f425d5d3724a80b17
Ahmad Fernando <ahmadfernando88@gmail.com>
2025-06-04 22:21:11 +0700

    perbaikan

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,11 +19,26 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        //     $guards = empty($guards) ? [null] : $guards;
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+        //     foreach ($guards as $guard) {
+        //         if (Auth::guard($guard)->check()) {
+        //             return redirect(RouteServiceProvider::HOME);
+        //         }
+        //     }
+
+        //     return $next($request);
+        $guard = $guards[0] ?? null;
+
+        if (Auth::guard($guard)->check()) {
+            $user = Auth::user();
+
+            if ($user->role === 'admin') {
+                return redirect('/admin/dashboard');
+            } elseif ($user->role === 'super-admin') {
+                return redirect('/super-admin/dashboard');
+            } else {
+                return redirect('/dashboard');
             }
         }
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Guest\BeritaController as GuestBeritaController;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
 // Tambahkan use controller yang kamu pakai di route
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\Admin\AsetController;
@@ -30,6 +33,27 @@ Route::get('/', function () {
 });
 
 Route::get('/detail/{id}', [GuestBeritaController::class, 'show'])->name('detail-berita');
+
+Route::post('/logout', function () {
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/');
+})->name('logout')->middleware('auth');
+
+Route::get('/login', function (Request $request) {
+    if (Auth::check()) {
+        $user = Auth::user();
+        // Redirect sesuai role
+        if ($user->role === 'admin') {
+            return redirect('/admin/dashboard');
+        } elseif ($user->role === 'super admin') {
+            return redirect('/super-admin/dashboard');
+        } else {
+            return redirect('/dashboard');
+        }
+    }
+    return view('auth.login'); // tampilan login biasa kalau belum login
+})->middleware('guest')->name('login');
 
 // Group untuk admin, tanpa Route::namespace()
 Route::middleware(['auth', 'checkrole:admin'])->name('admin.')->group(function () {
